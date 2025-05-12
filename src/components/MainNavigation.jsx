68c80df48383aa4d790a16a9fd0b3ccaf01e4714
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-12 01:37:16 -0700

    Added hamburger for responsive

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,54 +1,63 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
+
 export default function MainNavigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen((prev) => !prev);
+  }
+
   return (
-    <>
-      <header className={classes.header}>
-        <nav className={classes.navbar}>
-          <ul>
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? classes.active : undefined
-                }
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/gallery"
-                className={({ isActive }) =>
-                  isActive ? classes.active : undefined
-                }
-              >
-                Gallery
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) =>
-                  isActive ? classes.active : undefined
-                }
-              >
-                About Me
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/contact"
-                className={({ isActive }) =>
-                  isActive ? classes.active : undefined
-                }
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header className={classes.header}>
+      <nav className={classes.navbar}>
+        <button className={classes.toggle} onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`${classes.navlist} ${isOpen ? classes.open : ""}`}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/gallery"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Gallery
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              About Me
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
