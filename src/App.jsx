6343b11c120cbb4040c80c412ecf96330f7aed53
Aaradhya Github <aaradhyagithub@gmail.com>
2025-12-31 22:49:48 -0800

    test comment

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ export default function App() {
         },
         { path: "about", element: <About /> },
         { path: "contact", element: <Contact /> }
+        //test
       ]
     }
   ]);
